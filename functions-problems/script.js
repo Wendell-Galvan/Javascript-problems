7796bd9fb28140146635c55bf10fd74bb695742e
Wendell <wmgalvan6@gmail.com>
2022-06-15 14:31:47 -0700

    Functions problems 1 initial commit

---
+++ functions-problems/script.js
@@ -0,0 +1,29 @@
+//Write a JavaScript function that accepts a string as a parameter and find the longest word within the string.
+
+/*
+step 1: create a variable with a string to be used as a parameter
+step 2: create a function that will loop through string array
+step 3: create a variable in the function that turns the string into an array
+step 4: create a variable to be used for longest word
+step 5: create an if statement to check if length of array is > than longest word
+*/
+
+let string = "This is a string to be analyzed";
+
+function findLongestWord (str){
+	let stringArray = str.split(" ");
+	let longestWord = "";
+
+	for (let i= 0; i < stringArray.length; i++){
+		if (stringArray.length[i] > longestWord.length){
+			longestWordstring = stringArray[i];
+		}
+	}
+
+	return longestWord;
+}
+
+
+console.log(findLongestWord(string));
+
+console.log(string);
