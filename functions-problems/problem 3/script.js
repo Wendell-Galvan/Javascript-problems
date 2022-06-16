78c9da6a8803936b0e828ad1f914049a1577c5c3
Wendell <wmgalvan6@gmail.com>
2022-06-15 21:13:02 -0700

    final commit problem 3

--- functions-problems/problem 3/script.js
+++ functions-problems/problem 3/script.js
@@ -3,11 +3,11 @@
 
 /*
 step 1: create function that accepts two parameters, a string and a letter
-step 2: create a variable to count the number of letter
-step 3: 
-step 4: 
-step 5: 
-step 6: 
+step 2: create a variable to count the number of specified letter
+step 3: create loop to loop through string 
+step 4: in if statemen: use charAt() method to iterate through string array and find specified letter
+step 5: Add to letter counter if specified letter found
+step 6: return letterCounter and call on function with string and a letter as parameter. 
 */
 
 let string = "This is a string to be analyzed";
